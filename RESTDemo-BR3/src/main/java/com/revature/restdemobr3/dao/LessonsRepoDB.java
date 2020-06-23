eaaee898b81ffd9e0c3d95313cb00ce2f8a30e57
jorlando989 <jorlando989@gmail.com>
2020-06-23 16:35:48 -0400

    add index and css

--- RESTDemo-BR3/src/main/java/com/revature/restdemobr3/dao/LessonsRepoDB.java
+++ RESTDemo-BR3/src/main/java/com/revature/restdemobr3/dao/LessonsRepoDB.java
@@ -1,6 +1,5 @@
 package com.revature.restdemobr3.dao;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
--- RESTDemo-BR3/src/main/java/com/revature/restdemobr3/web/TrainingService.java
+++ RESTDemo-BR3/src/main/java/com/revature/restdemobr3/web/TrainingService.java
@@ -10,38 +10,39 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.revature.restdemobr3.dao.LessonsRepoDB;
+import com.revature.restdemobr3.dao.StudentsRepoDB;
 import com.revature.restdemobr3.model.Lesson;
 import com.revature.restdemobr3.model.Student;
 import com.revature.restdemobr3.model.Trainer;
-import com.revature.restdemobr3.dao.LessonsRepoDB;
 
 @Path("/service")
 public class TrainingService {
 
-	ITrainerRepo trainerRepo = new TrainerRepoDB();
+//	TrainerRepoDB trainerRepo = new TrainerRepoDB();
 	LessonsRepoDB lessonRepo = new LessonsRepoDB();
-	IStudentRepo studentRepo = new StudentRepoDB();
+//	StudentsRepoDB studentRepo = new StudentsRepoDB();
 	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response addLesson(Lesson lesson) {
-		lessonRepo.addLesson(lesson);
-		return Response.ok().build();
-	}
+//	@POST
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public Response addLesson(Lesson lesson) {
+//		lessonRepo.addLessons(lesson);
+//		return Response.ok().build();
+//	}
 	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response addStudent(Student student) {
-		studentRepo.addStudent(student);
-		return Response.ok().build();
-	}
+//	@POST
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public Response addStudent(Student student) {
+//		studentRepo.addNewStudent(student);
+//		return Response.ok().build();
+//	}
 	
-	@GET
-	@Path("/gettrainers")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAllTrainers() {
-		return Response.ok((ArrayList<Trainer>)trainerRepo.getAllTrainers()).build();
-	}
+//	@GET
+//	@Path("/gettrainers")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response getAllTrainers() {
+//		return Response.ok((ArrayList<Trainer>)trainerRepo.getAllTrainers()).build();
+//	}
 	
 	@GET
 	@Path("/getlessons")
@@ -50,18 +51,18 @@ public class TrainingService {
 		return Response.ok((ArrayList<Lesson>)lessonRepo.getAllLessons()).build();
 	}
 	
-	@GET
-	@Path("/getstudents")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAllStudents() {
-		return Response.ok((ArrayList<Student>)studentRepo.getAllStudents()).build();
-	}
+//	@GET
+//	@Path("/getstudents")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response getAllStudents() {
+//		return Response.ok((ArrayList<Student>)studentRepo.getAllStudents()).build();
+//	}
 	
-	@GET
-	@Path("/getlessonranks")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getLessonRanking() {
-		return Response.ok((ArrayList<Lesson>)lessonRepo.getBestLessons()).build();
-	}
+//	@GET
+//	@Path("/getlessonranks")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response getLessonRanking() {
+//		return Response.ok((ArrayList<Lesson>)lessonRepo.getBestLessons()).build();
+//	}
 	
 }
